196db40bb7411b436f45b23ba194fa6fe7a1be37
PierrunoYT <pierrebruno@hotmail.ch>
2025-08-02 11:42:06 +0200

    feat(install): add deepspeed windows wheel installation and update start command
    Add installation of deepspeed Windows wheel from GitHub releases to support Windows environments. Update start command to use specific app.py file instead of generic placeholder.

--- install.js
+++ install.js
@@ -19,6 +19,7 @@ module.exports = {
       params: {
         venv: "env",                // Edit this to customize the venv folder path
         message: [
+          "uv pip install https://github.com/6Morpheus6/deepspeed-windows-wheels/releases/download/v0.17.5/deepspeed-0.17.5+e1560d84-cp310-cp310-win_amd64.whl",
           "uv pip install -r requirements.txt"
         ],
       }
