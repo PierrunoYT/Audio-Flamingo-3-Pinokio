a1a5a60583da7d6a25a4aa75566c4209c498c445
PierrunoYT <pierrebruno@hotmail.ch>
2025-12-15 17:41:03 +0100

    fix(start): update regex pattern for monitoring shell events

--- start.js
+++ start.js
@@ -15,7 +15,7 @@ module.exports = {
           // The regular expression pattern to monitor.
           // When this pattern occurs in the shell terminal, the shell will return,
           // and the script will go onto the next step.
-          "event": "/Running on (http:\\/\\/[^\\s]+)/",   
+          "event": "/http:\\/\\/[^\\s\\/]+:\\d{2,5}(?=[^\\w]|$)/",   
 
           // "done": true will move to the next step while keeping the shell alive.
           // "kill": true will move to the next step after killing the shell.
